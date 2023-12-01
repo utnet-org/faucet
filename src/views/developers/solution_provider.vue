5cbacf3ca31c59a285940da8a2a7ff1f7b41ab13
LJWstudyaa <2583892238@qq.com>
2023-12-01 19:18:09 +0800

    New changes

--- src/views/developers/solution_provider.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="content">
-        <div>{{ $t('nav.Solution_Provider') }}</div>
-    </div>
-</template>
-<style scoped lang="less"></style>
--- src/views/soloutions/hashing_power.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="content">
-        <div>{{ $t('nav.Hashing_Power') }}</div>
-    </div>
-</template>
-<style scoped lang="less"></style>
